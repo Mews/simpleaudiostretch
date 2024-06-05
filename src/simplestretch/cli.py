6cf229f0050a389b34963b1bc60178a290b8828e
Mews <ar754456@gmail.com>
2024-06-05 18:32:56 +0100

    Added command line interface to stretch audio

---
+++ src/simplestretch/cli.py
@@ -0,0 +1,18 @@
+from . import stretch_audio, speedup_audio
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser("Stretch an audio file or speed it up or down")
+
+    parser.add_argument('-a', '--audio', required=True, type=str, help="Path to the audio file")
+    parser.add_argument('-f', '--factor', required=True, type=float, help="Factor for the change in audio length/speed")
+    parser.add_argument('-o', '--output', required=True, type=str, help="Path for the output file")
+    parser.add_argument('-s', '--speed', action='store_true', help="If this is passed, the factor will be applied to the speed of the audio rather than the length")
+
+    args = parser.parse_args()
+
+    if args.speed:
+        speedup_audio(audio=args.audio, factor=args.factor, output=args.output)
+    
+    else:
+        stretch_audio(audio=args.audio, factor=args.factor, output=args.output)
