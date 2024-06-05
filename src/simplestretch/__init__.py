924863ec490be77f5756988e674c09a025dda261
Mews <ar754456@gmail.com>
2024-06-05 08:28:14 +0100

    The factor argument in stretch_audio now represents the ratio to the new length rather than speed

--- src/simplestretch/__init__.py
+++ src/simplestretch/__init__.py
@@ -3,14 +3,21 @@ from numpy import ndarray
 from typing import Optional, Union, Tuple
 
 def stretch_audio(audio:Union[str, ndarray], factor:float, output:Optional[str] = None, samplerate:Optional[int] = None) -> Tuple[ndarray, int]:
+
+
+    #Type checks
+    if not (isinstance(audio, str) or isinstance(audio, ndarray)):
+        raise TypeError("\'audio\' must be the path to a audio file or a numpy ndarray")
+
+    if factor <= 0:
+        raise ValueError("\'factor\' must be greater than 0")
+
+
     #If a file path is provided, load it as a ndarray using soundfile
     if isinstance(audio, str):
         audio, samplerate = soundfile.read("song.mp3")
-
-    elif not isinstance(audio, ndarray):
-        raise TypeError("\'audio\' must be the path to a audio file or a numpy ndarray")
-
-    stretched_samplerate = round(samplerate/(1/factor))
+    
+    stretched_samplerate = round(samplerate/factor)
 
     if isinstance(output, str):
         try:
