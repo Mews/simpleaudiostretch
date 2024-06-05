58a3ea3e1a533cb3f4f4aabaad911d204f790079
Mews <ar754456@gmail.com>
2024-06-05 10:09:19 +0100

    Added files for docs building

---
+++ sphinx_conf.py
@@ -0,0 +1,33 @@
+#Tell sphinx and rtd where to find source code
+import sys, pathlib
+sys.path.append((pathlib.Path(__file__).parent.parent.parent / "src").resolve().as_posix())
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'simpleaudiostretch'
+copyright = '2024, Mews'
+author = 'Mews'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import sphinx_rtd_theme
+
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
