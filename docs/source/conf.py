b9e038134f25287bf668f03d05612c16a24a161a
Mews <ar754456@gmail.com>
2024-06-05 10:41:28 +0100

    Mock imports when building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme", "sphinx.ext.autosummary"
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+autodoc_mock_imports = ["numpy", "soundfile"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
